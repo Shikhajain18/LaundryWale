98a388df31d892d026ee7ceee82d6873f473f3f9
Shikha Jain <shikhajain181199@gmail.com>
2020-12-12 12:50:08 +0530

    ui created and firebase setup

---
+++ app/src/main/java/team/laundrywale/androidcustomerapp/Common/Common.java
@@ -0,0 +1,13 @@
+package team.laundrywale.androidcustomerapp.Common;
+
+
+
+
+public class Common {
+
+
+    public static final String user_customer_tbl = "CustomersInformation";
+    public static final String token_tbl = "Tokens";
+
+
+}
---
+++ app/src/main/java/team/laundrywale/androidcustomerapp/OrderActivity.java
@@ -0,0 +1,53 @@
+package team.laundrywale.androidcustomerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import team.laundrywale.androidcustomerapp.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    public static int displayHour;
+    public static int displayMinute;
+    public static int displayDayOfMonth;
+    public static String displayMonth = " ";
+    public static int displayYear;
+    public static int totalAmount;
+    public static String orderNumberToShow = " ";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        TextView orderNo = findViewById(R.id.orderNo);
+        TextView eta = findViewById(R.id.eta);
+        TextView total = findViewById(R.id.total);
+
+
+        boolean fromBar = getIntent().getBooleanExtra("from_bar", false);
+
+        if(!fromBar){
+            Bundle bundle = getIntent().getExtras();
+            displayHour = bundle.getInt("display_hour");
+            displayMinute=  bundle.getInt("display_min");;
+            displayDayOfMonth = bundle.getInt("display_day");;
+            displayMonth = bundle.getString("display_month");
+            displayYear = bundle.getInt("display_year");;
+            totalAmount = bundle.getInt("total_amount");
+            orderNumberToShow = "456";
+
+            orderNo.setText("Order No. : " + orderNumberToShow);
+            eta.setText("Delivery ETA  : " + "0"+displayHour+":"+displayMinute+" "+displayDayOfMonth+" "+displayMonth+" "+displayYear);
+            total.setText("Total Amount: rs."+totalAmount);
+
+        }else{
+            orderNo.setText("Order No. : " + orderNumberToShow);
+            eta.setText("Delivery ETA  : " + "0"+displayHour+":"+displayMinute+" "+displayDayOfMonth+" "+displayMonth+" "+displayYear);
+            total.setText("Total Amount: rs."+totalAmount);
+        }
+
+    }
+}
